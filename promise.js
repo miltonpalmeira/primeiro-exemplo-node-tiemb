8f5d9c5c5946e28f6d0b116ccde9c661dddfe1e8
Milton Palmeira <lpsantana_@hotmail.com>
2024-05-20 09:23:21 -0300

    first commit

---
+++ promise.js
@@ -0,0 +1,25 @@
+function sum(x) {
+    return new Promise((resolve, reject) => {
+        if (Number(x) == NaN || typeof x != "number") {
+            reject('Digite um número corretamente.');
+        }
+        setTimeout(() => {
+            resolve(x + 500);
+        }, 3000);
+    });
+}
+
+// sum('b').then((resultado) => {
+//     console.log(resultado);
+// });
+
+async function main() {
+    try {
+        const resultado = await sum(100);
+        console.log(resultado);
+    } catch (error) {
+        console.log(`Error: ${error}`);
+    }
+}
+
+main();
